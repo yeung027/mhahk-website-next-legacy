4efb6ac27febb3034d4d3bdf41e2cd978cd0602f
unknown <shinghei.yeung@gmail.com>
2025-02-11 17:55:13 +0800

    search bar with react-icons

--- frontend/app/[lang]/components/PageHeader.tsx
+++ frontend/app/[lang]/components/PageHeader.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { LiaSearchSolid } from "react-icons/lia";
 
 interface PageHeaderProps {
     pathname: string
@@ -7,8 +8,11 @@ interface PageHeaderProps {
 export default function PageHeader({ pathname } : PageHeaderProps) {
     return (
         <header className={"h-[49px] w-full bg-mainGreen hidden xl:flex flex-row-reverse px-[47px] items-center text-white text-[0.938rem]"}>
-            <div className="ml-[33px]">
-                <input className="text-white bg-[#61b99f] h-[27px] rounded-[3px]" type='search' />
+            <div className="ml-[33px] flex flex-row-reverse">
+                <input className="text-white bg-[#4db093] focus:bg-[#61b99f] h-[27px] rounded-[3px] px-[7px] w-[124px]" type='search' />
+                <div className='absolute'>
+                    <LiaSearchSolid className='h-[27px] text-[1.3rem] text-white mr-[7px] cursor-pointer' />
+                </div>
             </div>
             <div className="">
                 <Link href={`zh-HK/${pathname}`}>
