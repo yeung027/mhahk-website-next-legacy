59a198662170e423a5d7b1936bf0d23c80808342
yeung027 <shinghei.yeung@gmail.com>
2025-03-02 09:18:57 +0800

    grid category list css tidy & show title line2

--- frontend/app/[lang]/components/index/index-grid-category-list.tsx
+++ frontend/app/[lang]/components/index/index-grid-category-list.tsx
@@ -34,43 +34,67 @@ export default function IndexGridCategoryList({ pathname, items } : IndexGridCat
     };
 
     return (
-            <section className={"flex flex-col w-full mt-[20px] xl:grid xl:grid-cols-3 gap-[3vw] xl:gap-4"}>
-
+            <section className=" 
+                flex flex-col w-full mt-[20px]
+                xl:grid xl:grid-cols-3 gap-[3vw] xl:gap-4
+            ">
                 {items &&
-                items.map((item, index) => {
-                    const isDivVisible = useIsVisible({ current: divsRef.current[index] }, 0.5);
-                    const outter_class      = `bg-cover bg-[image:var(--bgm)] w-full aspect-7_69_1 font-[500] text-[20px]`
-                    const inner_class       = `px-[6vw]`
-                    const span_class        = ``
-                    const xl_outter_class   = `xl:bg-[image:var(--bg)] ${notoSansHK.className} ${isVisible[index] ? "opacity-100 scale-[1]" : "opacity-0 scale-[0.9]"} delay-[0] transition duration-[0.8s] ease-in-out xl:h-[170px] flex text-[#00a0e9] xl:font-[500] xl:text-[36px]`
-                    const xl_inner_class    = `xl:px-0 xl:self-end xl:mb-[12px] xl:ml-[30px] xl:h-[80px] xl:w-[60%] flex`
-                    const xl_span_class     = `delay-[0.6s] transition duration-[1.5s] ease-in-out flex self-center ${isVisible[index] ? "opacity-100 translate-x-[0]" : "opacity-0 -translate-x-[20px]"}`
-
-                    useEffect(() => {
-                        handleVisibilityChange(index, isDivVisible);
-                    }, [isDivVisible, index]);
-                    return  <div 
+                    items.map((item, index) => {
+                        const isDivVisible = useIsVisible({ current: divsRef.current[index] }, 0.5);
+                        
+                        useEffect(() => {
+                            handleVisibilityChange(index, isDivVisible);
+                        }, [isDivVisible, index]);
+            
+                        return (
+                            <div 
                                 ref={(el) => {
                                     divsRef.current[index] = el!;
                                 }}
-                                className={`${outter_class} ${xl_outter_class}`}
+                                className={`
+                                    bg-cover bg-[image:var(--bgm)] w-full aspect-7_69_1 
+                                    font-[500] text-[1.25rem] leading-[1.25rem] 
+                                    ${item.title2? 'xl:text-[1.8rem] xl:font-[500] xl:leading-[37px]' : 'xl:text-[2.25rem] xl:font-[500] xl:leading-[80px]'}
+                                    xl:bg-[image:var(--bg)] ${notoSansHK.className}
+                                    ${isVisible[index] ? "opacity-100 scale-[1]" : "opacity-0 scale-[0.9]"}
+                                    delay-[0] transition duration-[0.8s] ease-in-out xl:h-[170px] flex text-[#00a0e9]
+                                    
+                                `}
                                 style={{
                                     //@ts-ignore
-                                    '--bg': `url(${item.bg_image? `${process.env.NEXT_PUBLIC_STRAPI_API_URL}${item.bg_image.url}` : ""})`,
-                                    '--bgm': `url(${item.bg_image_mobile? `${process.env.NEXT_PUBLIC_STRAPI_API_URL}${item.bg_image_mobile.url}` : ""})`,
-                                  }}
+                                    '--bg': `url(${item.bg_image ? `${process.env.NEXT_PUBLIC_STRAPI_API_URL}${item.bg_image.url}` : ""})`,
+                                    '--bgm': `url(${item.bg_image_mobile ? `${process.env.NEXT_PUBLIC_STRAPI_API_URL}${item.bg_image_mobile.url}` : ""})`,
+                                }}
                                 key={`grid-category-item-${index}`}
                             >
-                            <div className={`${inner_class} ${xl_inner_class}`}>
-                                <span className={`${span_class} ${xl_span_class}`}>
-                                 {item.title}
-                                </span>
+                                <div className={` 
+                                        px-[2vw] xl:px-0
+                                        ml-[4vw]
+                                        xl:self-end 
+                                        ${item.title2? 'xl:ml-[30px] xl:mb-[8px] ' : 'xl:ml-[30px] xl:mb-[12px] '} 
+                                        xl:h-[80px] xl:w-[60%] 
+                                        flex flex-row xl:flex-col
+                                    `}
+                                >
+                                    <span className={` 
+                                        delay-[0.6s] transition duration-[1.5s] ease-in-out flex self-center xl:self-start
+                                        ${isVisible[index] ? "opacity-100 translate-x-[0]" : "opacity-0 -translate-x-[20px]"}
+                                    `}>
+                                        {item.title}
+                                    </span>
+                                    {item.title2 &&
+                                        <span className={` xl:ml-[10px]
+                                            delay-[0.6s] transition duration-[1.5s] ease-in-out flex self-center xl:self-start
+                                            ${isVisible[index] ? "opacity-100 translate-x-[0]" : "opacity-0 -translate-x-[20px]"}
+                                        `}>
+                                            {item.title2}
+                                        </span>
+                                    }
+                                </div>
                             </div>
-                            </div>
-                            
-                })
+                        );
+                    })
                 }
             </section>
-    
-    );
+            );
 }
