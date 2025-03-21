41f6d53386c7ce301be7bb2d7df44c87f36b42ed
root <shinghei.yeung@gmail.com>
2025-03-21 06:34:44 +0000

    commit from ssh dev server

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     images: {
-        domains: ["localhost"], // 允許本機開發環境
+        domains: ["localhost", "159.89.204.9"], // 允許本機開發環境
       },
 };
 
